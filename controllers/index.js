0c6aadf4bf07931c5cc984170958c9c814ac6344
Hendersonville <hendersonville@Hendersonvilles-MacBook-Pro.local>
2021-02-01 16:54:18 -0600

    read-list-routes and ReadList model created to allow user to add posts to their read list

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const readListRoutes = require('./read-list-routes');
 /* const dashboardRoutes = require('./dashboard-routes.js'); */
 
 router.use('/', homeRoutes);
 /* router.use('/dashboard', dashboardRoutes); */
 router.use('/api', apiRoutes);
+router.use('/read-list', readListRoutes);
 
 module.exports = router;
