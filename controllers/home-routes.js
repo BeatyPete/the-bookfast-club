044b0f536d45c0bb1b9fba3944a163a270dd394c
BeatyPete <akira941@gmail.com>
2021-01-31 23:35:13 -0600

    made main views

---
+++ controllers/home-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Upvote } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+          'id',
+          'title',
+          'cover_img_url',
+          'author',
+          'publish_date',
+          'isbn',
+          'description',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM upvote WHERE post.id = upvote.post_id)'), 'upvote_count']
+        ],
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+        /* loggedIn: req.session.loggedIn */
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/* router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+}); */
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-/* const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
+/* const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
@@ -21,16 +21,16 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers'); */
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create(/* { helpers } */);
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars'); */
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-/* app.use(express.static(path.join(__dirname, 'public'))); */
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
