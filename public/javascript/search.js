5bd3ae59481a6e55ee2ea9c46af3c0166b096553
BeatyPete <akira941@gmail.com>
2021-02-02 05:56:32 -0600

    added some styling to searched books and added tailwind npm package

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,4 +1,30 @@
+/* these two click functions animate the search sidebar */
 $("#search-close-btn").click(function(){
+    closeSearch()
+});
+$("#book-select").click(function(){
+    closeSearch()
+});
+
+$("#book-search").submit(function(){
+    openSearch()
+});
+
+const openSearch = () => {
+    event.preventDefault()
+    $("#search-background").animate({
+        opacity: '.75'
+    });
+    $("#search").animate({
+        maxWidth: "100%",
+    }, {
+        /* this start function should call before the animation starts but only works the second time */
+        start: function() {
+            $("#whole").removeClass("hidden");
+        }
+    });
+}
+const closeSearch = () => {
     $("#search-background").animate({
         opacity: '0'
     });
@@ -9,19 +35,4 @@ $("#search-close-btn").click(function(){
             $("#whole").addClass("hidden");
         }
     });
-
-});
-
-$("#book-search").submit(function(){
-    event.preventDefault()
-    $("#search-background").animate({
-        opacity: '.75'
-    });
-    $("#search").animate({
-        maxWidth: "100%",
-    }, {
-        start: function() {
-            $("#whole").removeClass("hidden");
-        }
-    });
-});
+}
