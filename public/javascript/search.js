f2304331760240229ef17cbbe7ece1c90082bf91
Brock <bruceanthonyii@gmail.com>
2021-02-04 18:43:18 -0600

    added colors to results

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -83,7 +83,7 @@ const displayResults = function(bookData) {
         const btnContainerEl = $("<div>")
         btnContainerEl.addClass("flex justify-end")
         const btnEl = $("<button>")
-        btnEl.addClass("book-select group relative py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")
+        btnEl.addClass("book-select group relative py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black primary-button hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-200")
         btnEl.attr("id", bookData.items[i].id)
         btnEl.text("Recommend this book")
         btnContainerEl.append(btnEl)
