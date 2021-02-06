e84690ef11f0bfeb2897c56b91656ff1550f9da1
kohringsw <kohringsw@gmail.com>
2021-02-06 14:51:23 -0600

    update contolers/api/readlist-routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-const readListRoutes = require('./api/readlist-routes.js');
 
 router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
--- public/javascript/readlist.js
+++ public/javascript/readlist.js
@@ -3,32 +3,3 @@ $(".fav-btn").on("click", function () {
   addToReadList();
 });
 
-async function addToReadList(bookData) {
-    // these const decide what is posted to the readlist. const isbn is a link to the book.
-    const title = `${bookData.volumeInfo.title}`
-    if (!bookData.volumeInfo.imageLinks) {
-        var cover_img_url = '/images/no-img.jpg';
-    } else {
-        var cover_img_url = `${bookData.volumeInfo.imageLinks.thumbnail}`
-    }
-    const author = `${bookData.volumeInfo.authors}`
-    const isbn = `${bookData.volumeInfo.infoLink}`
-    const response = await fetch(`/api/readlist`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            cover_img_url,
-            author,
-            isbn
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        location.reload();
-    } else {
-        alert(response.statusText);
-    }
-}
